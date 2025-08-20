ba86554a31533a6e98421ea59c942a7b5fa8d4ae
DaijobuDes <git@daijobudes.net>
2025-08-20 17:19:58 +0800

    Add database seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +16,22 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => '1234',
         ]);
+
+        for ($i = -5; $i <= 5; $i++) {
+            $date = now()->addDays($i)->startOfDay();
+
+            Task::factory(10)->create([
+                'user_id' => $user,
+                'task_date' => $date,
+                'content' => fake()->text(),
+                'is_finished' => fake()->boolean(33),
+                'position' => 0,
+            ]);
+        }
     }
 }
