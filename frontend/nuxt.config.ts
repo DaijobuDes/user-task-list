9f9fe17e34976de0db3a388e9e3c9b0929847997
DaijobuDes <git@daijobudes.net>
2025-08-22 12:28:44 +0800

    Setup NuxtJS config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint'],
+  devServer: {
+    host: '100.72.41.21',
+    port: 3000,
+    cors: {
+      origin: '*',
+    }
+  }
 })
