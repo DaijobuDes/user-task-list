122f8751e7c46604cb20a6daf8af94ead30c7acf
DaijobuDes <git@daijobudes.net>
2025-08-22 18:31:22 +0800

    Add sidebar, add store for calendar state

---
+++ frontend/components/DatePicker.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Calendar } from '@/components/ui/calendar'
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+} from '@/components/ui/sidebar'
+
+import { useCalendarStore } from '@/stores/useCalendarStore';
+const calendar = useCalendarStore()
+// import { getLocalTimeZone, parseDate, today, type DateValue } from '@internationalized/date'
+
+// // const date = ref<DateValue>(today(getLocalTimeZone()))
+// const date = ref<DateValue>(parseDate("2025-08-02"))
+
+// watch(date, (newDate) => {
+//     console.log(newDate.toString())
+// })
+
+</script>
+
+<template>
+  <SidebarGroup class="px-0">
+    <SidebarGroupContent>
+      <Calendar v-model="calendar.date" class="[&_[role=gridcell].bg-accent]:bg-sidebar-primary [&_[role=gridcell].bg-accent]:text-sidebar-primary-foreground [&_[role=gridcell]]:w-[33px]" />
+    </SidebarGroupContent>
+  </SidebarGroup>
+</template>
---
+++ frontend/pages/hometest.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useAuthStore } from "@/stores/useAuthStore"
+
+const auth = useAuthStore()
+const router = useRouter()
+
+if (!auth.isAuthenticated) {
+  router.push('/')
+}
+
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
